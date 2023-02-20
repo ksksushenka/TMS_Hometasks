2e642002b45d5ff88a8f8a58a18d61639556efeb
Kseniya Grebenyuk <kgrebenyuk@caeonline.com>
2023-02-20 13:38:21 -0600

    Hello World and Hello user.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Введите ваше имя");
+
+            string user = Console.ReadLine();
+
+            Console.WriteLine($"Hello {user}");
+
+        }
+    }
+}
