ebf628602cbcc26948f6c1c115e16a0f6cfb6250
Kseniya Grebenyuk <kgrebenyuk@caeonline.com>
2023-03-31 10:36:08 -0500

    Added task1 + event in task2 (Lesson11).

---
+++ HelloWorld/HelloWorld/Hometask_Lesson11/Lesson11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMS_Hometasks.Hometask_Lesson11.Task1;
+using static TMS_Hometasks.Hometask_Lesson11.Task1.PriceMonitor;
+
+namespace TMS_Hometasks.Hometask_Lesson11
+{
+    internal class Lesson11
+    {
+        public static void ShowPriceMessage(int price)
+        {
+            Console.WriteLine("Good price.");
+        }
+
+        public static void Lesson11_task1()
+        {
+            Random random = new Random();
+
+            DelegatePriceMonitor nameDelegate = ShowPrice;
+            Notify += ShowPriceMessage;
+
+            nameDelegate(random.Next(1, 100));
+
+            PriceMonitor monitor = new PriceMonitor(ShowPrice);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Hometask_Lesson11/Task1/PriceMonitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_Hometasks.Hometask_Lesson11.Task1
+{
+    internal class PriceMonitor
+    {
+        public delegate void DelegatePriceMonitor(int price);
+        public static event DelegatePriceMonitor Notify;
+
+        public PriceMonitor(DelegatePriceMonitor delegatePriceMonitor) 
+        {
+        }
+
+        public static void ShowPrice(int price)
+        {
+            if (price < 10)
+            {
+                Notify(price);
+            }
+
+            Console.WriteLine($"Average price is {price}.");
+        }
+    }
+}
